cd80e65f1cf3da2b6ed94e0818fdb8998f9e5ca2
sunny <sunnysun2108@gmail.com>
2018-11-16 13:40:35 -0500

    preloading lazy-loading modules after app loaded.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule,PreloadAllModules } from '@angular/router';
 
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { SignupComponent } from './auth/signup/signup.component';
@@ -17,8 +17,13 @@ const appRoutes: Routes = [
 ]
 
 @NgModule({
-	imports: [RouterModule.forRoot(appRoutes)], 
-	/*we should pass the appRoute to configure this router module that angular ships with. with that now, our router now configured
+	//preloading strategy, basic preload all lazy loading modules 
+	// after app loaded.
+
+	imports: [RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules} )], 
+
+	/*
+	we should pass the appRoute to configure this router module that angular ships with. with that now, our router now configured
 	and then get this module into our main module by export this regular configured module
 	*/
 	exports: [RouterModule]
